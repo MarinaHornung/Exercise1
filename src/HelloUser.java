9782b4700b476a7be37f9d05a53f52581f59db14
MarinaHornung <marina.hornung@tum.de>
2017-11-03 16:06:40 +0100

    Hello User is added

--- src/HelloUser.java
+++ src/HelloUser.java
@@ -1,4 +1,16 @@
 
-public class HelloUser {
+import java.util.Scanner;
 
+public class HelloUser {
+String userName;
+public static void main(String userName) {
+
+	Scanner scanner1= new Scanner(System.in);
+	
+	System.out.print("Enter your name: ");
+	userName = scanner1.nextLine();
+}
+public static void greetUser(String userName) {
+	System.out.println("Hello"+userName+"!");
+}
 }
