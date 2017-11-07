b2e1f1758d49a366564fb8f511ea0123e6d9b2ad
MarinaHornung <marina.hornung@tum.de>
2017-11-07 19:45:07 +0100

    HelloUser has been added

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,21 @@
 
+import java.util.Scanner;
+
 public class HelloWorld {
 
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello ASE2017 - How are you now? :)");
+		
+		System.out.println("Enter your name:");
+		Scanner input= new Scanner(System.in);
+		String userName = input.next();
+		
+		HelloUser user1 = new HelloUser(userName);
+		user1.greetUser();
+		
+		
 	}
 
 }
